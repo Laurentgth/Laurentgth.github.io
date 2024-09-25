f56be7fad6e3c6da2fab38e1be09fab7d9ff48e1
Laurent Gauthier <laurentgth@gmail.com>
2024-09-25 22:55:02 +0200

    Proof year display.

--- js/main.js
+++ js/main.js
@@ -1,7 +1,13 @@
 const footerYearElement = document.querySelector('.footer__year');
 
-document.addEventListener('DOMContentLoaded', () => {
+const displayYear = () => {
   const year = new Date();
   footerYearElement.textContent = year.getFullYear().toString();
   footerYearElement.style.display = 'inline';
-});
+};
+
+if (document.readyState === 'loading') {
+  document.addEventListener('DOMContentLoaded', displayDate);
+} else {
+  doSomething();
+}
